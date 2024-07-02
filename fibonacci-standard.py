7e64cacd2a2787155d4ced985da32954515c183d
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-07-02 10:14:48 -0400

    Add more methods for conditions on standard

--- fibonacci-standard.py
+++ fibonacci-standard.py
@@ -1,10 +1,29 @@
-def fibonacci(i: int) -> int:
+def fibonacci_if(i: int) -> int:
     if i == 0:
         return 0
     elif i == 1:
         return 1
     else:
-        return fibonacci(i - 1) + fibonacci(i - 2)
+        return fibonacci_if(i - 1) + fibonacci_if(i - 2)
 
 
-print([*map(fibonacci, range(10))])
+def fibonacci_if_golf(i: int) -> int:
+    if i > 1:
+        return fibonacci_if_golf(i - 1) + fibonacci_if_golf(i - 2)
+    else:
+        return i
+
+
+def fibonacci_match(i: int) -> int:
+    match i:
+        case 0:
+            return 0
+        case 1:
+            return 1
+        case _:
+            return fibonacci_match(i - 1) + fibonacci_match(i - 2)
+
+
+print([*map(fibonacci_if, range(10))])
+print([*map(fibonacci_if_golf, range(10))])
+print([*map(fibonacci_match, range(10))])
